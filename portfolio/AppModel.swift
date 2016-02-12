0db2261d080b71040e6bdd03a658f47b35a9e358
Kulraj Singh <koolraj003@gmail.com>
2016-02-12 18:58:04 +0530

    started portfolio collection screen

---
+++ portfolio/AppModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppModel.swift
+//  portfolio
+//
+//  Created by iOS Developer on 12/02/16.
+//  Copyright © 2016 Xperts Infosoft. All rights reserved.
+//
+
+import UIKit
+
+class AppModel: BaseModel {
+
+}
